3c5ad5e5b557b363ac8dcb213000ebad92c9fc9b
mu <muflexnet@gmail.com>
2014-03-14 13:00:41 +0000

    restored on win7-now with compass config,properly pulls in susy and compani

---
+++ .compass.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+require 'companimation'
+require 'susy'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = ".tmp/styles"
+sass_dir = "app/styles"
+images_dir = "images"
+fonts_dir = "styles/fonts"
+javascripts_dir = "scripts/vendor"
--- Gruntfile.js
+++ Gruntfile.js
@@ -136,26 +136,15 @@ module.exports = function (grunt) {
       }
     },
     compass: {
-        options: {
-            require: ['susy','companimation'],
-            sassDir: '<%= yeoman.app %>/styles',
-            cssDir: '.tmp/styles',
-            generatedImagesDir: '.tmp/images/generated',
-            imagesDir: '<%= yeoman.app %>/images',
-            javascriptsDir: '<%= yeoman.app %>/scripts',
-            fontsDir: '<%= yeoman.app %>/styles/fonts',
-            importPath: '<%= yeoman.app %>/bower_components',
-            httpImagesPath: '/images',
-            httpGeneratedImagesPath: '/images/generated',
-            httpFontsPath: '/styles/fonts',
-            relativeAssets: false
-        },
-        dist: {},
-        server: {
-            options: {
-                debugInfo: true
-            }
-        }
+      options: {
+        config: '.compass.rb'
+      },
+      dist: {},
+      server: {
+          options: {
+              debugInfo: true
+          }
+      }
     },
 
     // not used since Uglify task does concat,
